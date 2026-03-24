6ebcfdf42f5b9733a668b4ebc336cf7bb31ea2d9
BismuthBloom <BismuthBloom@gmail.com>
2026-03-24 09:53:32 -0400

    index and tilemap file

---
+++ scripts/tilemap.js
@@ -0,0 +1,4 @@
+// tldr if local data, render. need to import tiles first to make the associations 
+// otherwise spin up local data and render b001
+// 14 w, 9 height
+// either do on click querying for tiles here or just something else idk. id can be used like int-int for the x-y coord
