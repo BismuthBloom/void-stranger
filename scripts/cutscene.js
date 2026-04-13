3061dd61e74d7ae9eb9e35b03bd8cb8f3cc05a98
BismuthBloom <BismuthBloom@gmail.com>
2026-04-13 16:31:43 -0400

    Cutscene engine file. Not implemented

---
+++ scripts/cutscene.js
@@ -0,0 +1,28 @@
+let data;
+
+// load the cutscene layout for the game
+
+// activate cutscene
+// call clear board function from here, need a div that'll sit above the white tiles
+
+// progress text
+
+/*
+async function gatherJSON() {
+	// get the script
+	try {
+		const response = await fetch("data/birch.json");
+		if (!response.ok) { throw new Error("can't find the json"); }
+
+		data = await response.json();
+		addTiles(data);
+	}
+	catch (error) {
+		console.error("failed to fetch:", error);
+	}
+}
+
+$(document).ready( function() {
+	gatherJSON();
+});
+*/
